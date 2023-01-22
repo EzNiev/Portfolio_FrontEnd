a304d28ab1b7bb43005c91f835d37d057b05995a
EzNiev <ezequiel.nieva1998@gmail.com>
2023-01-22 16:50:19 -0300

    Creacion carpeta backend
    Se empieza a agregar el maven papaaa

---
+++ Portfolio_NievaGabrielEzequiel/BackEnd/ezniev/src/test/java/com/portfolio/ezniev/EznievApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ezniev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EznievApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
