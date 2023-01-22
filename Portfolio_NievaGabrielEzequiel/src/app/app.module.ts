9adb3952c646c4458da68bf24b8785b18b76900d
EzNiev <ezequiel.nieva1998@gmail.com>
2023-01-22 02:15:10 -0300

    Elimino componentes viejos
    Al implementar el logoap y redes en navbar elimino lso componentes viejos.

--- Portfolio_NievaGabrielEzequiel/src/app/app.module.ts
+++ Portfolio_NievaGabrielEzequiel/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciasComponent } from './components/experiencias/experiencias.component';
@@ -22,8 +20,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent,
-    RedesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciasComponent,
