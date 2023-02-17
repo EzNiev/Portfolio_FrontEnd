045b8dcc00c2cdcd49b6107a52f186444a87c141
EzNiev <ezequiel.nieva1998@gmail.com>
2023-02-17 01:21:34 -0300

    Update s-experiencia.service.ts
    Ahora hay que hacer que ande el formulario

--- Portfolio_NievaGabrielEzequiel/FrontEnd/src/app/service/s-experiencia.service.ts
+++ Portfolio_NievaGabrielEzequiel/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  expURL = 'http://localhost:8080/experiencia/'
+  expURL = 'http://localhost:8080/experiencia/';
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Experiencia[]>{
@@ -23,7 +23,7 @@ export class SExperienciaService {
   }
 
   public update(id: number, experiencia: Experiencia): Observable<any>{
-    return this.httpClient.put<any>(this.expURL + `update/${id}`, experiencia)
+    return this.httpClient.put<any>(this.expURL + `update/${id}`, experiencia);
   }
 
   public delete(id: number): Observable<any>{
