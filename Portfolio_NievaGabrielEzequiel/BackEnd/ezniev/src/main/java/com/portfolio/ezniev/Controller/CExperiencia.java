81367aaa029d6d2da55cca89b646a581288b07d0
EzNiev <ezequiel.nieva1998@gmail.com>
2023-02-17 00:43:50 -0300

    Update CExperiencia.java
    ANDA LA CARGA DE EXPERIENCIAS QUIERO LLORAR.

--- Portfolio_NievaGabrielEzequiel/BackEnd/ezniev/src/main/java/com/portfolio/ezniev/Controller/CExperiencia.java
+++ Portfolio_NievaGabrielEzequiel/BackEnd/ezniev/src/main/java/com/portfolio/ezniev/Controller/CExperiencia.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/experiencia")
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class CExperiencia {
-
     @Autowired
     SExperiencia sExperiencia;
 
